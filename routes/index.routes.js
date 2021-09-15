66cdff3d8a246a100e2b2f4d312184986ca135b1
Jean-Marie BERNARD <jm11bernard@gmail.com>
2021-09-15 17:38:19 +0200

    routing_project OK

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const usersRouter = require('./users.routes');
+const projectsRouter = require('./projects.routes');
 
 router.use('/users', usersRouter);
+router.use('/projects', projectsRouter);
 
 module.exports = router;
