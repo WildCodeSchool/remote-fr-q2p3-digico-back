dca4955b49f2fbf4d9b8837b2105ae67f9c3aa7e
Jean-Marie BERNARD <jm11bernard@gmail.com>
2021-09-15 10:44:30 +0200

    data.sql_udpate

--- data.sql
+++ data.sql
@@ -1,16 +1,142 @@
-DROP TABLE IF EXISTS users;
-
-CREATE TABLE users (
-    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    username varchar(255) NOT NULL,
-    password varchar(255) NOT NULL,
-    email varchar(255) NOT NULL
+CREATE TABLE `users` (
+    `id` INT  NOT NULL ,
+    `pseudonyme` VARCHAR(100)  NOT NULL ,
+    `firstname` VARCHAR(150)  NOT NULL ,
+    `lastname` VARCHAR(150)  NOT NULL ,
+    `email` VARCHAR(100)  NOT NULL ,
+    `mobile` VARCHAR(100)  NOT NULL ,
+    `user_img` VARCHAR(250)  NOT NULL ,
+    `adress` VARCHAR(150)  NOT NULL ,
+    `socials` VARCHAR(150)  NOT NULL ,
+    `competences` VARCHAR(150)  NOT NULL ,
+    `desc` VARCHAR(1500)  NOT NULL ,
+    `experience_points` INT  NOT NULL ,
+    `projects_id` INT  NOT NULL ,
+    `ideas_id` INT  NOT NULL ,
+    `is_admin` BOOLEAN  NOT NULL ,
+    PRIMARY KEY (
+        `id`
+    )
 );
 
-INSERT INTO users
-VALUES (
-    1,
-    'admin',
-    'test',
-    'admin@example.com'
+CREATE TABLE `projects` (
+    `id` INT  NOT NULL ,
+    `title` VARCHAR(100)  NOT NULL ,
+    `desc` VARCHAR(1500)  NOT NULL ,
+    `socials` VARCHAR(150)  NOT NULL ,
+    `img` VARCHAR(150)  NOT NULL ,
+    `localisation` VARCHAR(150)  NOT NULL ,
+    `project_date` DATE  NOT NULL ,
+    `owner_id` INT  NOT NULL ,
+    PRIMARY KEY (
+        `id`
+    )
 );
+
+CREATE TABLE `ideas` (
+    `id` INT  NOT NULL ,
+    `title` VARCHAR(100)  NOT NULL ,
+    `desc` VARCHAR(840)  NOT NULL ,
+    `socials` VARCHAR(150)  NOT NULL ,
+    `img` VARCHAR(150)  NOT NULL ,
+    `idea_date` DATE  NOT NULL ,
+    `owner_id` INT  NOT NULL ,
+    PRIMARY KEY (
+        `id`
+    )
+);
+
+CREATE TABLE `categories` (
+    `id` INT  NOT NULL ,
+    `projects_id` INT  NOT NULL ,
+    `idea_id` INT  NOT NULL ,
+    `categorie` VARCHAR(150)  NOT NULL ,
+    PRIMARY KEY (
+        `id`
+    )
+);
+
+CREATE TABLE `users_tags` (
+    `id` INT  NOT NULL ,
+    `tag_names` VARCHAR(100)  NOT NULL ,
+    PRIMARY KEY (
+        `id`
+    )
+);
+
+CREATE TABLE `comments` (
+    `id` INT  NOT NULL ,
+    `comment_content` VARCHAR(480)  NOT NULL ,
+    `comment_date` DATE  NOT NULL ,
+    `idea_id` INT  NOT NULL ,
+    `writer_id` INT  NOT NULL ,
+    PRIMARY KEY (
+        `id`
+    )
+);
+
+CREATE TABLE `badges` (
+    `id` INT  NOT NULL ,
+    `title` VARCHAR(100)  NOT NULL ,
+    `badges` VARCHAR(100)  NOT NULL ,
+    `badge_img` VARCHAR(100)  NOT NULL ,
+    PRIMARY KEY (
+        `id`
+    )
+);
+
+CREATE TABLE `user_badges` (
+    `badges_id` INT  NOT NULL ,
+    `user_id` INT  NOT NULL 
+);
+
+CREATE TABLE `user_tag` (
+    `tag_id` INT  NOT NULL ,
+    `user_id` INT  NOT NULL 
+);
+
+CREATE TABLE `category_tag` (
+    `categories_tag_id` INT  NOT NULL ,
+    `categories_id` INT  NOT NULL 
+);
+
+CREATE TABLE `categories_tag` (
+    `id` INT NOT NULL,
+    `category_tag_names` VARCHAR(100),
+    PRIMARY KEY (
+        `id`
+    )
+);
+
+ALTER TABLE `users` ADD CONSTRAINT `fk_users_projects_id` FOREIGN KEY(`projects_id`)
+REFERENCES `projects` (`id`);
+
+ALTER TABLE `users` ADD CONSTRAINT `fk_users_ideas_id` FOREIGN KEY(`ideas_id`)
+REFERENCES `ideas` (`id`);
+
+ALTER TABLE `categories` ADD CONSTRAINT `fk_categories_projects_id` FOREIGN KEY(`projects_id`)
+REFERENCES `projects` (`id`);
+
+ALTER TABLE `categories` ADD CONSTRAINT `fk_categories_idea_id` FOREIGN KEY(`idea_id`)
+REFERENCES `ideas` (`id`);
+
+ALTER TABLE `comments` ADD CONSTRAINT `fk_comments_idea_id` FOREIGN KEY(`idea_id`)
+REFERENCES `ideas` (`id`);
+
+ALTER TABLE `user_badges` ADD CONSTRAINT `fk_user_badges_badges_id` FOREIGN KEY(`badges_id`)
+REFERENCES `badges` (`id`);
+
+ALTER TABLE `user_badges` ADD CONSTRAINT `fk_user_badges_user_id` FOREIGN KEY(`user_id`)
+REFERENCES `users` (`id`);
+
+ALTER TABLE `user_tag` ADD CONSTRAINT `fk_user_tag_tag_id` FOREIGN KEY(`tag_id`)
+REFERENCES `users_tags` (`id`);
+
+ALTER TABLE `user_tag` ADD CONSTRAINT `fk_user_tag_user_id` FOREIGN KEY(`user_id`)
+REFERENCES `users` (`id`);
+
+ALTER TABLE `category_tag` ADD CONSTRAINT `fk_category_tag_categories_tag_id` FOREIGN KEY(`categories_tag_id`)
+REFERENCES `categories_tag` (`id`);
+
+ALTER TABLE `category_tag` ADD CONSTRAINT `fk_category_tag_categories_id` FOREIGN KEY(`categories_id`)
+REFERENCES `categories` (`id`);
