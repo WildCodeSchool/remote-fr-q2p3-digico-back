574d08f83da693f33d61626658daaf8d516a6ed6
Jean-Marie BERNARD <jm11bernard@gmail.com>
2021-09-16 11:18:51 +0200

    categories routing ok

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const usersRouter = require('./users.routes');
 const ideasRouter = require('./ideas.routes');
 const projectsRouter = require('./projects.routes');
+const categoriesRouter = require('./categories.routes');
 
 router.use('/users', usersRouter);
 router.use('/ideas', ideasRouter);
 router.use('/projects', projectsRouter);
+router.use('/categories', categoriesRouter);
 
 module.exports = router;
