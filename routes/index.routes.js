c01acb676f48b148533704c9682b1364b9230045
Jean-Marie BERNARD <jm11bernard@gmail.com>
2021-09-16 14:47:44 +0200

    routing_categoreis_tag OK

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const usersRouter = require('./users.routes');
 const ideasRouter = require('./ideas.routes');
 const projectsRouter = require('./projects.routes');
+const categories_tagRouter = require('./categories_tag.routes');
 
 router.use('/users', usersRouter);
 router.use('/ideas', ideasRouter);
 router.use('/projects', projectsRouter);
+router.use('/categories_tag', categories_tagRouter);
 
-module.exports = router;
+module.exports = router; 
